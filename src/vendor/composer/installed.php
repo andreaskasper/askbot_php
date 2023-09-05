9bd76303127d810804717e41212460bcd95dfd5b
Andreas Kasper <djassi@web.de>
2023-09-05 13:44:29 +0000

    Refactoring everything into src. Updating jquery

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bea4e0dac8b8b1e244ff81e49f1613d169963856',
+    'name' => 'andreaskasper/askbot_php',
+  ),
+  'versions' => 
+  array (
+    'andreaskasper/askbot_php' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bea4e0dac8b8b1e244ff81e49f1613d169963856',
+    ),
+  ),
+);
